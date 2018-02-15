f96555e13fa278a315fcd77d87649410fbd4fd30
Mali Akmanalp <mali@akmanalp.com>
2018-02-15 02:59:17 -0500

    Add in helper classes that define message formats and also encoding/decoding helper functions.

---
+++ message.py
@@ -0,0 +1,74 @@
+import struct
+
+
+VERSION = b'\x01'
+
+
+def make_header(opcode, message_length, version=VERSION):
+    """Make a header binary string: version + length + opcode"""
+    return version + struct.pack('!I', 2 + message_length) + opcode
+
+
+def parse_header(binary_data, version=VERSION):
+    """Parse a header binary string: version + length + opcode"""
+
+    # Raise exception if version is wrong
+    curr_version = binary_data[0:1]
+    if curr_version != version:
+        raise ValueError("Protocol version {} is not what we are using ({}).".format(curr_version, version))
+
+    if len(binary_data) != 6:
+        raise ValueError("Header must be 6 bytes, got {}.".format(len(binary_data)))
+
+    message_length = struct.unpack("!I", binary_data[1:5])[0]
+    opcode = binary_data[5:6]
+
+    return opcode, message_length
+
+
+def make_message(message_type, *args, **kwargs):
+    """Make a message binary string: header + message"""
+
+    # Call the message class' encode function to do message-specific stuff
+    message_body = message_type._encode(*args, **kwargs)
+
+    # Option to not add header
+    if not kwargs.get("header", True):
+        return message_body
+
+    # Make a header for the message
+    message_length = len(message_body)
+    message_header = make_header(message_type.OPCODE, message_length)
+
+    return message_header + message_body
+
+
+class AbstractMessage(object):
+    """This is an abstract base class that handles encoding / decoding messages
+    using struct.pack, including the header. Look at subclasses for specific
+    message implementations."""
+
+    OPCODE = None
+    PACK_FORMAT = None
+
+    @classmethod
+    def _encode(cls, *args, **kwargs):
+        """Default implementation will look for PACK_FORMAT and do
+        struct.pack() with it and pass the arguments in. For more complicated
+        behavior you'll have to override this."""
+
+        if cls.PACK_FORMAT is None:
+            raise NotImplementedError("""The message class {} does not have a property called PACK_FORMAT""".format(cls))
+
+        return struct.pack(cls.PACK_FORMAT, *args, **kwargs)
+
+    @classmethod
+    def _decode(cls, binary_data):
+        """Default implementation will look for PACK_FORMAT and do
+        struct.unpack() with it and pass the arguments in. For more complicated
+        behavior you'll have to override this."""
+
+        if cls.PACK_FORMAT is None:
+            raise NotImplementedError("""The message class {} does not have a property called PACK_FORMAT""".format(cls))
+
+        return struct.unpack(cls.PACK_FORMAT, binary_data)
---
+++ protocol.py
@@ -0,0 +1,275 @@
+import struct
+
+import message
+from message import AbstractMessage, make_message
+
+#
+# Client-to-server Requests
+#
+
+class CreateRequest(AbstractMessage):
+    """Create account <username>"""
+
+    OPCODE = b'\x10'
+    PACK_FORMAT = "!100p"
+
+
+class DeleteRequest(AbstractMessage):
+    """Delete, only if logged in"""
+
+    OPCODE = b'\x20'
+    PACK_FORMAT = ""
+
+
+class LoginRequest(AbstractMessage):
+    """Login <username>, only if user exists"""
+
+    OPCODE = b'\x30'
+    PACK_FORMAT = "!100p"
+
+
+class LogoutRequest(AbstractMessage):
+    """Logout, only if logged in"""
+
+    OPCODE = b'\x40'
+    PACK_FORMAT = ""
+
+
+class SendMessageRequest(AbstractMessage):
+    """Send message <dest_username> <message>"""
+
+    OPCODE = b'\x50'
+    PACK_FORMAT = None
+
+    @classmethod
+    def _encode(cls, dest_account, text):
+        return struct.pack('!100p', bytes(dest_account, 'ascii')) +\
+               struct.pack('!255p', bytes(text, 'ascii'))
+
+    @classmethod
+    def _decode(cls, binary_data):
+        dest_account = struct.unpack('!100p', binary_data[:100])
+        text = struct.unpack('!255p', binary_data[100:])
+        return dest_account, text
+
+
+class CollectMessageRequest(AbstractMessage):
+    """Collect messages, only if logged in."""
+
+    OPCODE = b'\x60'
+    PACK_FORMAT = ""
+
+
+#
+# Server-to-client Responses
+#
+
+
+class CreateSuccessResponse(AbstractMessage):
+    OPCODE = b'\x11'
+    PACK_FORMAT = ""
+
+
+class CreateFailResponse(AbstractMessage):
+    OPCODE = b'\x12'
+    PACK_FORMAT = ""
+
+
+class DeleteSuccessResponse(AbstractMessage):
+    OPCODE = b'\x21'
+    PACK_FORMAT = ""
+
+
+class DeleteFailResponse(AbstractMessage):
+    OPCODE = b'\x22'
+    PACK_FORMAT = ""
+
+
+class LoginSuccessResponse(AbstractMessage):
+    OPCODE = b'\x31'
+    PACK_FORMAT = ""
+
+
+class LoginFailResponse(AbstractMessage):
+    OPCODE = b'\x32'
+    PACK_FORMAT = ""
+
+
+class LogoutSuccessResponse(AbstractMessage):
+    OPCODE = b'\x41'
+    PACK_FORMAT = ""
+
+
+class LogoutFailResponse(AbstractMessage):
+    OPCODE = b'\x42'
+    PACK_FORMAT = ""
+
+
+class SendSuccessResponse(AbstractMessage):
+    OPCODE = b'\x51'
+    PACK_FORMAT = ""
+
+
+class SendFailResponse(AbstractMessage):
+    OPCODE = b'\x52'
+    PACK_FORMAT = ""
+
+
+class SendQueuedResponse(AbstractMessage):
+    """User offline but server queued the message"""
+    OPCODE = b'\x53'
+    PACK_FORMAT = ""
+
+
+class CollectSuccessResponse(AbstractMessage):
+    OPCODE = b'\x61'
+    PACK_FORMAT = ""
+
+
+class CollectFailResponse(AbstractMessage):
+    OPCODE = b'\x62'
+    PACK_FORMAT = ""
+
+
+class CollectNoNewResponse(AbstractMessage):
+    """No new messages to collect"""
+    OPCODE = b'\x63'
+    PACK_FORMAT = ""
+
+
+class UnknownMessageResponse(AbstractMessage):
+    OPCODE = b'\x99'
+    PACK_FORMAT = ""
+
+
+#
+# Lookup data structures to find messages easily
+#
+
+
+REQUEST_MESSAGES = [
+    CreateRequest,
+    DeleteRequest,
+    LoginRequest,
+    LogoutRequest,
+    SendMessageRequest,
+    CollectMessageRequest
+]
+
+RESPONSE_MESSAGES = [
+    CreateSuccessResponse,
+    CreateFailResponse,
+    DeleteSuccessResponse,
+    DeleteFailResponse,
+    LoginSuccessResponse,
+    LoginFailResponse,
+    SendSuccessResponse,
+    SendFailResponse,
+    SendQueuedResponse,
+    CollectSuccessResponse,
+    CollectFailResponse,
+    CollectNoNewResponse,
+    UnknownMessageResponse
+]
+
+MESSAGES = REQUEST_MESSAGES + RESPONSE_MESSAGES
+
+# To look up Messages by opcode
+opcode_table = {m.OPCODE: m for m in MESSAGES}
+
+
+def get_message_by_opcode(opcode):
+    return opcode_table.get(opcode, None)
+
+
+#
+# Message parsing / receiving functions
+#
+
+def receive_message(socket):
+    """Given a socket, read a message off of it: first the header, and then
+    using the size from the header, read the body."""
+
+    # Receive 6 bytes for the header
+    header = socket.recv(6)
+
+    # Parse header
+    opcode, message_length = message.parse_header(header)
+
+    # Receive the rest of the message based on the header
+    message_body = socket.recv(message_length)
+
+    # Look up opcode
+    message_type = get_message_by_opcode(opcode)
+
+    # Make sure opcode is valid
+    if message_type is None:
+        raise ValueError("Invalid opcode {}".format(opcode))
+
+    return message_type, message_type._decode(message_body)
+
+
+def parse_message(binary_data):
+    """This is mostly for testing, you'll probably need receive_message
+    instead."""
+
+    if len(binary_data) < 6:
+        raise ValueError("Your message is less than 6 bytes, meaning it can't fit a header and therefore can't be valid")
+
+    message_header = binary_data[:6]
+    message_body = binary_data[6:]
+
+    opcode, message_length = message.parse_header(message_header)
+    message_type = get_message_by_opcode(opcode)
+
+    if message_type is None:
+        raise ValueError("Invalid opcode {}".format(opcode))
+
+    return message_type, message_type._decode(message_body)
+
+
+
+# import myServerReceive
+# request_handlers = {
+    # Request.CREATE: myServerReceive.create_request,
+    # b'\x20': myServerReceive.delete_request,
+    # b'\x30': myServerReceive.login_request,
+    # b'\x40': myServerReceive.logout_request,
+    # b'\x50': myServerReceive.send_message_request,
+    # b'\x60': myServerReceive.collect_messages,
+# }
+
+# import myClientReceive
+# response_handlers = {
+    # # Create_success
+    # b'\x11': myClientReceive.create_success,
+    # # Create failure
+    # b'\x12': myClientReceive.general_failure,
+    # # Delete success
+    # b'\x21': myClientReceive.delete_success,
+    # # Delete failure
+    # b'\x22': myClientReceive.general_failure,
+    # # Login success
+    # b'\x31': myClientReceive.login_success,
+    # # Login failure
+    # b'\x32': myClientReceive.general_failure,
+    # # Logout success
+    # b'\x41': myClientReceive.logout_success,
+    # # Logout failure
+    # b'\x42': myClientReceive.general_failure,
+    # # Send message success
+    # b'\x51': myClientReceive.send_message_success,
+    # # Send message failure
+    # b'\x52': myClientReceive.general_failure,
+    # # Collect messages success
+    # b'\x61': myClientReceive.collect_messages_success,
+    # # Collect messages failure
+    # b'\x62': myClientReceive.general_failure,
+
+
+    # # Unknown opcode
+    # b'\x71': myClientReceive.unknown_opcode,
+
+    # # No new messages
+    # b'\x72': myClientReceive.no_new_messages
+# }
